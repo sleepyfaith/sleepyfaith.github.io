dba96e218137e9546297eba5c99e295f1f10c0ce
sleepyfaith <67561809+sleepyfaith@users.noreply.github.com>
2025-11-20 01:07:47 +0000

    improve mobile popup, link to faith.lgbt/test on main page

--- test/js/popupWindow.js
+++ test/js/popupWindow.js
@@ -11,7 +11,10 @@ export class PopupWindow extends Window {
         const content = document.createElement("div");
         content.classList.add("content");
 
+        // main message for the pop up
+
         const msg = document.createElement("p");
+        
 
         if (type == "link") {
             msg.textContent = locale.popup.link;
@@ -22,9 +25,14 @@ export class PopupWindow extends Window {
             msg.setAttribute("data-i18n", "popup.file");
 
         }
+        content.appendChild(msg);
 
+
+        // yes no buttons
         const buttons = document.createElement("div");
         buttons.classList.add("popup-buttons");
+
+
 
         const confirmBtn = document.createElement("button");
         confirmBtn.textContent = locale.popup.yes;
@@ -38,8 +46,51 @@ export class PopupWindow extends Window {
 
         buttons.appendChild(confirmBtn);
         buttons.appendChild(cancelBtn);
-        
-        content.appendChild(msg);
+
+
+
+        // browser selector for mobile popup, will add more browsers later
+        if (!window.matchMedia("(min-width: 768px)").matches) {
+            const browserContainer = document.createElement("div")
+            browserContainer.classList.add("browser")
+
+            const browserInfo = document.createElement("div");
+            browserInfo.classList.add("browser-info")
+
+            let ua = navigator.userAgent
+            let browser = "chrome" // assume user is on chrome before checking due to market share.
+
+            // update browser if user agent contains its info
+            if (/Firefox/i.test(ua)) {
+                browser = "firefox";
+            } else if (/Edg/i.test(ua)) {
+                browser = "edge";
+            } else if (/Safari/i.test(ua) && !/Chrome/i.test(ua)) {
+                browser = "safari";
+            }
+
+            const browserImg = document.createElement("img")
+            browserImg.classList.add("browser-img")
+            browserImg.src = `assets/browsers/${browser}.svg`
+            browserInfo.appendChild(browserImg)
+            
+
+            const browserName = document.createElement("p")
+            browserName.textContent = browser
+            browserName.classList.add("browser-name")
+            browserInfo.appendChild(browserName)
+
+            browserContainer.appendChild(browserInfo)
+
+            const browserRadio = document.createElement("input")
+            browserRadio.type = "radio"
+            browserRadio.checked = true
+            browserContainer.appendChild(browserRadio)
+
+
+            content.appendChild(browserContainer)
+        }
+
         content.appendChild(buttons)
         
         super("popup", locale.popup.title, content)
